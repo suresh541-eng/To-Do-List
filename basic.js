c4655cea30d5252803de284c715fe7b327723e79
unknown <ullankulasuresh@gmail.com>
2025-08-13 11:20:52 +0530

    complete

--- basic.js
+++ basic.js
@@ -1,29 +1,37 @@
-function addTask() {
-  let input = document.getElementById("taskInput");
+function addTask() {        //function declaration
 
-  let li = document.createElement("li");
+  let input = document.getElementById("taskInput"); //input tag in call chesanu 
 
+  let li = document.createElement("li"); //li tag create
+
+  // checkbox 
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.onclick = function () {
-    li.classList.toggle("completed", checkbox.checked);
+  checkbox.onclick = function () {    // function expression
+    li.classList.toggle("complete", checkbox.checked);//If true: complete class is added, If false: complete class is removed.
   };
 
+  // span text
   let span = document.createElement("span");
-  span.textContent = input.value;
+  span.textContent = input.value;   //the user typed in the input box (taskInput) as the text inside that <span>.
 
+  // remove button
   let delBtn = document.createElement("button");
-  delBtn.textContent = "Delete";
-  delBtn.className = "delete-btn";
-  delBtn.onclick = function () {
+  delBtn.textContent = "Delete";      // which content display
+  delBtn.className = "delete-btn";  // class name of the button 
+  delBtn.onclick = function () {    // function expression
     li.remove();
   };
 
+  // checkbox + text + delete button li ki appending
   li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(delBtn);
 
+
+  // li append the taskList (ul)
   document.getElementById("taskList").appendChild(li);
 
-  input.value = "";
-}
+  input.value = "";   //Assigning an empty string ""
+
+};
